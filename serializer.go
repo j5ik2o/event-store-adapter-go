9ffd69e5755bc248c6e05c600cbbdb3a83502efb
Junichi Kato <j5ik2o@gmail.com>
2023-10-18 10:30:07 +0900

    fix: serializer type name

--- serializer.go
+++ serializer.go
@@ -2,9 +2,9 @@ package event_store_adapter_go
 
 import "encoding/json"
 
-type JsonEventSerializer struct{}
+type DefaultEventSerializer struct{}
 
-func (s *JsonEventSerializer) Serialize(event Event) ([]byte, error) {
+func (s *DefaultEventSerializer) Serialize(event Event) ([]byte, error) {
 	result, err := json.Marshal(event)
 	if err != nil {
 		return nil, &SerializationError{EventStoreBaseError{"Failed to serialize the event", err}}
@@ -12,7 +12,7 @@ func (s *JsonEventSerializer) Serialize(event Event) ([]byte, error) {
 	return result, nil
 }
 
-func (s *JsonEventSerializer) Deserialize(data []byte, eventMap *map[string]interface{}) error {
+func (s *DefaultEventSerializer) Deserialize(data []byte, eventMap *map[string]interface{}) error {
 	err := json.Unmarshal(data, eventMap)
 	if err != nil {
 		return &DeserializationError{EventStoreBaseError{"Failed to deserialize the event", err}}
@@ -20,9 +20,9 @@ func (s *JsonEventSerializer) Deserialize(data []byte, eventMap *map[string]inte
 	return nil
 }
 
-type JsonSnapshotSerializer struct{}
+type DefaultSnapshotSerializer struct{}
 
-func (s *JsonSnapshotSerializer) Serialize(aggregate Aggregate) ([]byte, error) {
+func (s *DefaultSnapshotSerializer) Serialize(aggregate Aggregate) ([]byte, error) {
 	result, err := json.Marshal(aggregate)
 	if err != nil {
 		return nil, &SerializationError{EventStoreBaseError{"Failed to serialize the snapshot", err}}
@@ -30,7 +30,7 @@ func (s *JsonSnapshotSerializer) Serialize(aggregate Aggregate) ([]byte, error) 
 	return result, nil
 }
 
-func (s *JsonSnapshotSerializer) Deserialize(data []byte, aggregateMap *map[string]interface{}) error {
+func (s *DefaultSnapshotSerializer) Deserialize(data []byte, aggregateMap *map[string]interface{}) error {
 	err := json.Unmarshal(data, aggregateMap)
 	if err != nil {
 		return &DeserializationError{EventStoreBaseError{"Failed to deserialize the snapshot", err}}
